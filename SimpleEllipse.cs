0631bba3071beac3dba8db7cbc05fa3b8b72ef64
Aibekova <113463942+Aibekova@users.noreply.github.com>
2023-05-07 14:33:38 +0500

    Create SimpleEllipse.cs

---
+++ SimpleEllipse.cs
@@ -0,0 +1,13 @@
+using System; 
+using System.Windows; 
+using System.Windows.Media; 
+namespace Petzold.RenderTheGraphic 
+{     class SimpleEllipse : FrameworkElement     //класс является производным от FrameworkElement
+           {    protected override void OnRender (DrawingContext dc)    //переопределяет метод OnRender для получения DrawingContext
+                {          
+                    dc.DrawEllipse(Brushes.Blue, new Pen (Brushes.Red, 24),  //DrawingContext используется для рисования эллипса методом DrawEllipse              
+                    new Point(RenderSize.Width / 2,  RenderSize.Height / 2),                 
+                    RenderSize.Width / 2, RenderSize .Height / 2);         
+                 }     
+           }      
+}
