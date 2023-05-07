1bd6494e0c63b56b6fdccf3aadfc258774c844bc
Aibekova <113463942+Aibekova@users.noreply.github.com>
2023-05-07 18:23:38 +0500

    Create XamlCruncherSettings.cs

---
+++ XamlCruncherSettings.cs
@@ -0,0 +1,18 @@
+//глава 20
+using System; 
+using System.Windows;
+using System.Windows.Controls; 
+using System.Windows.Media; 
+namespace Petzold.XamlCruncher { 
+public class XamlCruncherSettings : Petzold .NotepadClone.NotepadCloneSettings   
+{        
+// Настройки пользователя по умолчанию       
+public Dock Orientation = Dock.Left;      
+public int TabSpaces = 4;      
+public string StartupDocument = "<Button xmlns=\"http://schemas .microsoft.com/winfx" + "/2006/xaml/presentation\" \r\n" +"      
+xmlns:x=\"http://schemas .microsoft.com/winfx" +  "/2006/xaml\">\r\n" + "    Hello, XAML!\r\n" +  "</Button>\r\n";    
+// Конструктор для инициализации настроек по умолчанию в NotepadCloneSettings. (изменяет шрифт по умолчанию на 10-точечный фиксированный шаг консоли Lucida.)        
+public XamlCruncherSettings()         {         
+FontFamily = "Lucida Console";    
+FontSize = 10 / 0.75;        
+}     } } 
